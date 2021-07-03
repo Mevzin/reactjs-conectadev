99b781e994a4e305b5f22ee8426382a5e8a0cbed
Thiago Torres de Souza <thiagomev@gmail.com>
2021-07-03 15:34:00 -0300

    Adicionado componente Header

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './style.css';
 
 function Home() {
     return (
         <div>
-            <header className="header">
-                <div className="toolbar">
-                    <div >
-                        <span>Conecta Dev</span>
-                    </div>
-                    <div className="">
-                        <button> Novo Post</button>
-                        <span>img1</span>
-                        <span>img2</span>
-                    </div>
-                </div>
-            </header>
+            <Header />
             <main className="main">
                 <div className="navbar">Navbar</div>
                 <div className="feed">feed</div>
