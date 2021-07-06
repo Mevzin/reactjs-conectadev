c258bbc26cd63ad4338d902030c7cdfe2f525cd5
Thiago Torres <thiagomev@gmail.com>
2021-07-06 14:28:38 -0300

    Adicionado sistema de rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import Home from './pages/home';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import SingIn from './pages/SingIn';
+import Home from './pages/home';
 import theme from './theme';
 
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home/>
-     </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route> 
+          <Route path="/sing-in">
+            <SingIn/>
+          </Route>
+          <Route path="*">
+            <h1>Page not found 404</h1>
+          </Route>
+        </Switch>
+    </Router>
+  </ThemeProvider>
   );
 }
 
---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SingIn() {
+    return (
+        <h1>Login Component</h1>
+    )
+}
+
+export default SingIn
